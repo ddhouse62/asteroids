03263a95ad9d9a6003f448193a09fcd8af34f51e
ddhouse62 <house.dalton62@gmail.com>
2024-12-07 10:43:59 -0600

    Adds player movement

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ def main():
         player1.update(dt)
         player1.draw(screen)
         pygame.display.flip()
-        
-        dt = clock.tick(60)
+
+        dt = clock.tick(60) / 1000
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
