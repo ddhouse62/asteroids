78babab22e31c42e476a1f3ab0df373b0b5d9f1b
ddhouse62 <house.dalton62@gmail.com>
2024-12-07 02:52:19 -0600

    Creates Game Loop Window

--- main.py
+++ main.py
@@ -5,7 +5,14 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting asteroids!")
+    pygame.init
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT)).fill(color="black")
+        pygame.display.flip()
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
