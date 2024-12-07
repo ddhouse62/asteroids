473d2d154783d2e7bf560a737dfd984411442506
ddhouse62 <house.dalton62@gmail.com>
2024-12-07 10:33:25 -0600

    Adds Player Sprite and Rotation

--- main.py
+++ main.py
@@ -3,22 +3,27 @@
 #throughout this file
 import pygame
 from constants import *
+#from circleshape import *
+from player import *
 
 def main():
     pygame.init
     clock = pygame.time.Clock()
     dt = 0
+    player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT)).fill(color="black")
+        screen.fill("black")
+        player1.update(dt)
+        player1.draw(screen)
         pygame.display.flip()
+        
         dt = clock.tick(60)
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
 
 
 
